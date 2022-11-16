1a860edf5f0e92cecc9a35d396b9668917431c00
islom <radjapov_islom@mail.ru>
2022-11-17 01:01:39 +0500

    traning

--- detect_gender_webcam.py
+++ detect_gender_webcam.py
@@ -12,7 +12,7 @@ import cvlib as cv
 # load model
 model = models.load_model('gender_detection.model')
 # open webcam
-webcam = cv2.VideoCapture(0)
+webcam = cv2.VideoCapture()
 
 # gender type
 classes = ['man','woman']
