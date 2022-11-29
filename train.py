599a826b1e51232c4ab64ef9faf1426ce89274d4
Islom-Radjapov <radjapov_islom@mail.com>
2022-11-29 14:22:53 +0500

    Traing

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 from keras.preprocessing.image import ImageDataGenerator
 from keras.optimizers import Adam
-from keras.preprocessing.image import img_to_array
+from keras.utils import img_to_array
 from keras.utils import to_categorical, plot_model
 from keras.models import Sequential
 from keras.layers import BatchNormalization, Conv2D, MaxPooling2D, Activation, Flatten, Dropout, Dense
@@ -17,20 +17,20 @@ import glob
 epochs = 100
 lr = 1e-3
 batch_size = 64
-img_dims = (96,96,3)
+img_dims = (96, 96, 3)
 
 data = []
 labels = []
 
 # load image files from the dataset
-image_files = [f for f in glob.glob(r'C:\Files\gender_dataset_face' + "/**/*", recursive=True) if not os.path.isdir(f)]
+image_files = [f for f in glob.glob(r'/Users/student/PycharmProjects/Detection/gender_dataset_face' + "/**/*", recursive=True) if not os.path.isdir(f)]
 random.shuffle(image_files)
 
 # converting images to arrays and labelling the categories
 for img in image_files:
 
     image = cv2.imread(img)
-    
+
     image = cv2.resize(image, (img_dims[0],img_dims[1]))
     image = img_to_array(image)
     data.append(image)
@@ -40,7 +40,7 @@ for img in image_files:
         label = 1
     else:
         label = 0
-        
+
     labels.append([label]) # [[1], [0], [0], ...]
 
 # pre-processing
@@ -48,13 +48,11 @@ data = np.array(data, dtype="float") / 255.0
 labels = np.array(labels)
 
 # split dataset for training and validation
-(trainX, testX, trainY, testY) = train_test_split(data, labels, test_size=0.2,
-                                                  random_state=42)
-
+(trainX, testX, trainY, testY) = train_test_split(data, labels, test_size=0.2, random_state=42)
 trainY = to_categorical(trainY, num_classes=2) # [[1, 0], [0, 1], [0, 1], ...]
 testY = to_categorical(testY, num_classes=2)
 
-# augmenting datset 
+# augmenting datset
 aug = ImageDataGenerator(rotation_range=25, width_shift_range=0.1,
                          height_shift_range=0.1, shear_range=0.2, zoom_range=0.2,
                          horizontal_flip=True, fill_mode="nearest")
@@ -64,15 +62,15 @@ def build(width, height, depth, classes):
     model = Sequential()
     inputShape = (height, width, depth)
     chanDim = -1
-
-    if K.image_data_format() == "channels_first": #Returns a string, either 'channels_first' or 'channels_last'
+    # return model
+    if K.image_data_format() == "channels_first": # Returns a string, either 'channels_first' or 'channels_last'
         inputShape = (depth, height, width)
         chanDim = 1
-    
-    # The axis that should be normalized, after a Conv2D layer with data_format="channels_first", 
+
+    # The axis that should be normalized, after a Conv2D layer with data_format="channels_first",
     # set axis=1 in BatchNormalization.
 
-    model.add(Conv2D(32, (3,3), padding="same", input_shape=inputShape))
+    model.add(Conv2D(32, (3, 3), padding="same", input_shape=inputShape))
     model.add(Activation("relu"))
     model.add(BatchNormalization(axis=chanDim))
     model.add(MaxPooling2D(pool_size=(3,3)))
@@ -119,7 +117,7 @@ model.compile(loss="binary_crossentropy", optimizer=opt, metrics=["accuracy"])
 
 # train the model
 H = model.fit_generator(aug.flow(trainX, trainY, batch_size=batch_size),
-                        validation_data=(testX,testY),
+                        validation_data=(testX, testY),
                         steps_per_epoch=len(trainX) // batch_size,
                         epochs=epochs, verbose=1)
 
@@ -130,10 +128,10 @@ model.save('gender_detection.model')
 plt.style.use("ggplot")
 plt.figure()
 N = epochs
-plt.plot(np.arange(0,N), H.history["loss"], label="train_loss")
-plt.plot(np.arange(0,N), H.history["val_loss"], label="val_loss")
-plt.plot(np.arange(0,N), H.history["acc"], label="train_acc")
-plt.plot(np.arange(0,N), H.history["val_acc"], label="val_acc")
+plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, N), H.history["acc"], label="train_acc")
+plt.plot(np.arange(0, N), H.history["val_acc"], label="val_acc")
 
 plt.title("Training Loss and Accuracy")
 plt.xlabel("Epoch #")
---
+++ work.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+
+
+plt.plot([96, 96], [96,96])
+plt.show()
